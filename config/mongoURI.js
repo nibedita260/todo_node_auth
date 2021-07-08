fb8b299910e1fc530368b194f08905d1e5eac221
Code1111 <deepi.nibedita260@gmail.com>
2021-07-08 17:00:43 +0530

    Update mongoURI.js

--- config/mongoURI.js
+++ config/mongoURI.js
@@ -1,3 +1,3 @@
-module.exports='mongodb+srv://nibi:Nibi@1146@cluster0.bnx8m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-GOOGLE_CLIENT_ID = '1011242423925-1jebq750vpcs8n063b5sbf4esi7gfjf6.apps.googleusercontent.com';
-GOOGLE_CLIENT_SECRET = 'hrbs6fXoj5q4tCZ8hsL20hT3';
+module.exports='mongodb+srv://<user>:<password>@cluster0.bnx8m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+GOOGLE_CLIENT_ID = XXXXXXXXXXXXXXXXXX;
+GOOGLE_CLIENT_SECRET = XXXXXXXXX;
